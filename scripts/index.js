5d283583344f0ae26570c958833a7b2cf35910ad
Renata <renata.arndt@gmail.com>
2025-08-14 16:38:43 -0300

    Corrige classes BEM

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,10 @@ let editPopupButton = document.querySelector(".profile__info_header_edit-button"
 let editPopup = document.querySelector("#edit-popup");
 let closeEditPopupButton = document.querySelector(".popup__container_close-button");
 let profileName = document.querySelector(".profile__info_header_name");
-let profileNameInput = document.querySelector(".popup__container_form_input-name");
+let profileNameInput = document.querySelector(".popup__container_form-input_name");
 let editForm = document.querySelector("#form");
 let profileDescription = document.querySelector(".profile__info_description");
-let profileDescriptionInput = document.querySelector(".popup__container_form_input-description");
+let profileDescriptionInput = document.querySelector(".popup__container_form-input_description");
 
 function openPopup() {
     editPopup.classList.add("popup_is-opened");
