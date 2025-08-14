c45a68677c9611132a706bf084846b8c15a7cc30
Renata <renata.arndt@gmail.com>
2025-08-14 16:25:23 -0300

    corrige css

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tripleten web_project_around
+
+Nesse projeto criamos, pela primeira vez, uma página com interação. 
+Usamos HTML para construir a estrutura do site e CSS para estilizar. 
+Nesse sprint aprendemos o básico do JavaScript, que para mim foi bastante desafiador, então usamos esses novos conhecimentos para criar um popup interativo no site. Ele aparece conforme clicamos no botão de editar perfil (localizado ao lado do nome). Abaixo coloco alguns prints para demonstrar:
--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,11 @@
 let editPopupButton = document.querySelector(".profile__info_header_edit-button");
 let editPopup = document.querySelector("#edit-popup");
-let closeEditPopipButton = document.querySelector(".popup__container_close-button");
+let closeEditPopupButton = document.querySelector(".popup__container_close-button");
 let profileName = document.querySelector(".profile__info_header_name");
 let profileNameInput = document.querySelector(".popup__container_form_input-name");
 let editForm = document.querySelector("#form");
 let profileDescription = document.querySelector(".profile__info_description");
 let profileDescriptionInput = document.querySelector(".popup__container_form_input-description");
-
 
 function openPopup() {
     editPopup.classList.add("popup_is-opened");
@@ -26,8 +25,9 @@ function handleEditForm(event) {
     profileName.textContent = profileNameInput.value;
 
     profileDescription.textContent = profileDescriptionInput.value;
+    closePopup();
 }
 
 editPopupButton.addEventListener("click", openPopup);
-closeEditPopipButton.addEventListener("click", closePopup);
+closeEditPopupButton.addEventListener("click", closePopup);
 editForm.addEventListener("submit", handleEditForm);
