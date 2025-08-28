790c2f744433e25d388ca63336d5d74377a45904
Renata <renata.arndt@gmail.com>
2025-08-28 14:50:31 -0300

    feat: adicionar popup para criar novo cartão

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Nesse projeto criamos, pela primeira vez, uma página com interação. 
 Usamos HTML para construir a estrutura do site e CSS para estilizar. 
 Nesse sprint aprendemos o básico do JavaScript, que para mim foi bastante desafiador, então usamos esses novos conhecimentos para criar um popup interativo no site. Ele aparece conforme clicamos no botão de editar perfil (localizado ao lado do nome). Abaixo coloco alguns prints para demonstrar:
+
+![pop-up aparecendo após clicar no botão](image-1.png)
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,50 @@
-let editProfileButton = document.querySelector(".profile__info_header_edit-button");
-let editPopup = document.querySelector("#edit-popup");
-let closePopupButton = document.querySelector(".popup__container_close-button");
-let profileName = document.querySelector(".profile__info_header_name");
-let profileInputName = document.querySelector("#name");
-let profileDescription = document.querySelector(".profile__info_description");
-let profileInputDescription = document.querySelector("#description");
-let editForm = document.querySelector("#form");
+const initialCards = [
+  {
+    name: "Yosemite National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+  },
+  {
+    name: "Blue Ridge Parkway",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+  },
+  {
+    name: "Sierra Nevada",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+  },
+  {
+    name: "Swiftcurrent Lake",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+  },
+  {
+    name: "Glacier National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
+  }
+];
+
+const editProfileButton = document.querySelector(".profile__info_header_edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const closeEditPopupButton = document.querySelector("#close-edit-popup");
+
+const profileName = document.querySelector(".profile__info_header_name");
+const profileInputName = document.querySelector("#name");
+const profileDescription = document.querySelector(".profile__info_description");
+const profileInputDescription = document.querySelector("#description");
+const editForm = document.querySelector("#form");
+
+const elementAddButton = document.querySelector(".profile__button");
+const addElementPopup = document.querySelector("#add-element-popup");
+const closePopupAddButton = document.querySelector("#close-add-popup");
+
+const addElementForm = document.querySelector("#add-form");
+const elementInputTitle = document.querySelector("#title");
+const elementInputLink = document.querySelector("#image-link");
+
+const elementTemplate = document.querySelector("#element-template");
+const elementCard = document.querySelector(".elements__card");
 
 function openPopup(){  
     editPopup.classList.add("popup_opened");
@@ -13,10 +52,15 @@ function openPopup(){
     profileInputName.value = profileName.textContent;
 
     profileInputDescription.value = profileDescription.textContent;
+
 }
 
-function closePopup(){
-    editPopup.classList.remove("popup_opened")
+function openAddElementPopup(){
+  addElementPopup.classList.add("popup_opened");
+}
+
+function closePopup(popup){
+    popup.classList.remove("popup_opened");
 }
 
 function handleEditForm(event){
@@ -24,11 +68,75 @@ function handleEditForm(event){
 
     profileName.textContent = profileInputName.value;
 
+  if(profileInputName.value.trim() ===  ""){
+    alert("Nome não pode estar vazio");
+    return;
+  }
+
     profileDescription.textContent = profileInputDescription.value;
 
-    closePopup();
+    if(profileDescription.value.trim() ===  ""){
+    alert("Descrição não pode estar vazia");
+    return;
+  }
+
+    closePopup(editPopup);
+}
+
+function handleClosePopup(event){
+  console.log(event.currentTarget);
+  if(
+    event.target === event.currentTarget ||
+    event.target.closest(".popup__container_close-button")
+  ){
+    closePopup(event.currentTarget);
+  }
+}
+
+function createElement ({name, link}){
+  const cloneElement = elementTemplate.content.cloneNode(true);
+  const imgLink = cloneElement.querySelector(".elements__card-image");
+  const title = cloneElement.querySelector(".elements__card_info-text");
+
+  if (imgLink){
+    imgLink.src = link;
+    imgLink.alt = name;
+  imgLink.setAttribute("loading", "lazy");
+  }
+
+  if(title){
+    title.textContent = name;
+  }
+  return cloneElement
+}
+
+function renderInitialElements(){
+  initialCards.forEach(createElement);
+}
+
+function handleAddElementForm(event){
+  event.preventDefault();
+
+  const elementTitle = elementInputTitle.value.trim();
+  const elementLink = elementInputLink.value.trim();
+
+  if(elementTitle === "" || elementLink === ""){
+    alert("Por favor, preencha todos os campos.");
+    return;
+  }
+
+  const newCard = createElement({name: elementTitle, link: elementLink});
+  elementCard.prepend(newCard);
+  
+  addElementForm.reset();
+  closePopup(addElementPopup);
 }
 
 editProfileButton.addEventListener("click", openPopup);
-closePopupButton.addEventListener("click", closePopup);
+elementAddButton.addEventListener("click", openAddElementPopup);
+
 editForm.addEventListener("submit", handleEditForm);
+addElementForm.addEventListener("submit", handleAddElementForm);
+
+editPopup.addEventListener("click", handleClosePopup);
+addElementPopup.addEventListener("click", handleClosePopup);
